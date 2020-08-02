e7011b88c695041de107c08998091c194960f8c9
bardromi <s2dtsxSy>
2020-08-02 11:50:55 +0300

    Added:  - User and Message models and migrations  - SocketIO for basic join and create message

---
+++ socketEvents.js
@@ -0,0 +1,39 @@
+const models = require("./models");
+
+const socketEvents = (io) => {
+    io.on('connection', (socket) => {
+        console.log(`New socket connection: SocketId: ${socket.id}`)
+
+        socket.on('disconnect', () => {
+            console.log(`SocketId: ${socket.id} disconnected`)
+        })
+
+        socket.on("join", async room => {
+            socket.join(room);
+            io.emit("roomJoined", room);
+        });
+
+        socket.on("message", async data => {
+            const {author, message} = data;
+            console.log(data);
+            await models.Message.create({
+                author,
+                message: message,
+            });
+
+            const latestMessages = await models.Message.findAll({
+                limit: 10,
+            });
+            io.emit("latestMessages", latestMessages);
+        });
+
+        //broadcast new message to all sockets
+        // socket.on('newMessage', (newMessage) => {
+        //     socket.broadcast.emit('addMessage', newMessage)
+        // })
+    });
+
+
+}
+
+module.exports = socketEvents
